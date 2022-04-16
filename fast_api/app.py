8f5185f59ddd3cfbbda1a25608ad562b3924c6ba
aNOOBis666 <mr.zooko@mail.ru>
2022-04-17 00:43:34 +0300

    models updated

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from sqlalchemy.orm import Session
 
 from database import crud, models, schemas
@@ -9,6 +11,20 @@ from database.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5500"
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Dependency
